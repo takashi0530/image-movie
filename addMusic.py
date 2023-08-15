51297babe02e40193ca815c3f3956fe6a647f60a
Takashi Nishimura <saishuheki.nisshing@gmail.com>
2023-08-15 16:44:15 +0900

    fix: タイプピントを追加

--- addMusic.py
+++ addMusic.py
@@ -4,6 +4,7 @@ import cv2
 import shutil
 import numpy as np
 from moviepy.editor import VideoFileClip, AudioFileClip, vfx
+from typing import List, Tuple, Union
 
 # Constants
 DESIRED_WIDTH = 749
@@ -13,7 +14,7 @@ AUDIO_PATH = "music/am.mp3"
 OUTPUT_VIDEO_NAME = "output_movie/movie.mp4"
 OUTPUT_VIDEO_WITH_MUSIC_NAME = "output_movie/music_movie.mp4"
 
-def rename_images(directory_path: str):
+def rename_images(directory_path: str) -> str:
     output_dir = os.path.join(directory_path, "result")
     os.makedirs(output_dir, exist_ok=True)
 
@@ -28,7 +29,7 @@ def rename_images(directory_path: str):
     print(f"Images have been copied with new names to {output_dir}.")
     return output_dir
 
-def resize_without_crop(img, desired_width, desired_height):
+def resize_without_crop(img: np.ndarray, desired_width: int, desired_height: int) -> np.ndarray:
     original_height, original_width = img.shape[:2]
     aspect_ratio = original_width / original_height
     desired_aspect_ratio = desired_width / desired_height
@@ -42,7 +43,7 @@ def resize_without_crop(img, desired_width, desired_height):
 
     return cv2.resize(img, (new_width, new_height), interpolation=cv2.INTER_AREA)
 
-def resize_and_center(img, desired_width, desired_height):
+def resize_and_center(img: np.ndarray, desired_width: int, desired_height: int) -> np.ndarray:
     resized_img = resize_without_crop(img, desired_width, desired_height)
     background = np.zeros((desired_height, desired_width, 3), dtype=np.uint8)
     y_offset = (desired_height - resized_img.shape[0]) // 2
@@ -50,22 +51,23 @@ def resize_and_center(img, desired_width, desired_height):
     background[y_offset:y_offset+resized_img.shape[0], x_offset:x_offset+resized_img.shape[1]] = resized_img
     return background
 
-def add_music_to_video(video_path, audio_path, output_path):
+def add_music_to_video(video_path: str, audio_path: str, output_path: str) -> None:
     video = VideoFileClip(video_path)
     audio = AudioFileClip(audio_path)
     video_duration = video.duration
+    # audio_repeated = audio.loop(duration=video_duration)  # Changed from `audio.fx(vfx.loop,...)` as it's more direct
     audio_repeated = audio.fx(vfx.loop, duration=video_duration)
     video_with_audio = video.set_audio(audio_repeated)
     video_with_audio.write_videofile(output_path, codec='libx264')
 
-def remove_file(filepath: str):
+def remove_file(filepath: str) -> None:
     if os.path.exists(filepath):
         os.remove(filepath)
         print(f"Removed file: {filepath}")
     else:
         print(f"File not found: {filepath}")
 
-def main():
+def main() -> None:
     data_path = os.path.join(IMAGE_DIR, '*.jpeg')
     files = sorted(glob.glob(data_path))
 
